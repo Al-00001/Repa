869d5ecf1e65da61584a2b69be0e24e77e845265
Al <AlMr@mail.ru>
2025-03-17 23:16:35 +0300

    Book_2 is 3

--- Book_2.py
+++ Book_2.py
@@ -1,12 +1,20 @@
-import random
-verbs = ['Leverage', 'Sync', 'Target', 'Gamify', 'Offline', 'Crowd-sourced', '24/7', 'Lean In', '30,000 foot']
-abjectives = ['A/B Tested', 'Freemium', 'Haperlocal', 'Siloed','B-to-B', 'Oriented', 'Claud-based', 'API-Based',]
-nouns = ['Erley Adopted', 'Low Hanging Fruit', 'Pipeline', 'Splah Page', 'Productiviti', 'Process', 'Triping point', 'Paradigm']
+# import random
+# verbs = ['Leverage', 'Sync', 'Target', 'Gamify', 'Offline', 'Crowd-sourced', '24/7', 'Lean In', '30,000 foot']
+# abjectives = ['A/B Tested', 'Freemium', 'Haperlocal', 'Siloed','B-to-B', 'Oriented', 'Claud-based', 'API-Based',]
+# nouns = ['Erley Adopted', 'Low Hanging Fruit', 'Pipeline', 'Splah Page', 'Productiviti', 'Process', 'Triping point', 'Paradigm']
+#
+# verb = random.choice(verbs)
+# abjective = random.choice(abjectives)
+# noun = random.choice(nouns)
+#
+# phrase = verb + ' ' + abjective + ' ' + noun
+#
+# print(phrase)
+try:
+    Name = input("Your Dog Name ? ")
+    Age = input("Your Dog Age ? ")
+    Age = int(Age)
+    print(f"{Name} Humen Age is {Age * 7}")
+except ValueError:
+    print("Error ! input data is vrong")
 
-verb = random.choice(verbs)
-abjective = random.choice(abjectives)
-noun = random.choice(nouns)
-
-phrase = verb + ' ' + abjective + ' ' + noun
-
-print(phrase)
